612fb61ce8f819cf9fc0054e7908c0aa28240f24
CepGamer <bolotov.s.s@yandex.ru>
2013-10-11 21:09:46 +0400

    Changed license

--- TrikRider/cyber.cpp
+++ TrikRider/cyber.cpp
@@ -1,28 +1,17 @@
 /*
-Copyright (c) 2013, Bolotov Sergey
-All rights reserved.
+Copyright 2013 Bolotov Sergey
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Saint-Petersburg State University nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Bolotov Sergey BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
 #include "cyber.h"
@@ -38,12 +27,6 @@ Cyber::Cyber(QObject *parent) :
     wheels = new QList<Motor *>();
     conServer = new QTcpServer ();
     buffer = new QString();
-
-
-    position.x = position.y = \
-            direction.x = direction.y = \
-            moving.x = moving.y = 0;        //  Resetting values
-//            acceleration.x = acceleration.y = 0;
 }
 
 vector operator *(matrix a, vector b)
@@ -173,7 +156,7 @@ void Cyber::setSettings()
     settings->setValue("DevPath", "/dev/i2c-2");
     settings->setValue("DevId", 72);        //0x48
     settings->setValue("ConnectionPort", 4444);
-    settings->setValue("Period", 0x2710);
+    settings->setValue("Period", 0x1000);
 
     printf("How many wheels do you have on your OMNI?");
     scanf("\n%c", &wheelSize);
@@ -362,10 +345,10 @@ void Cyber::checkPosition()
     //  Change direction
     absolute.tiltZ = kalmanCoef * (angles.tiltZ - correction) + (1 - kalmanCoef) * angVelocity;
     currRad += absolute.tiltZ / checksPerSecond;
-    if (count == checksPerSecond)
+    if (count == 0)
     {
-        while (currRad >  2 * Pi) currRad -= 2 * Pi;
-        while (currRad < -2 * Pi) currRad += 2 * Pi;
+        while (currRad > ( 2 * Pi)) currRad -= 2 * Pi;
+        while (currRad < (-2 * Pi)) currRad += 2 * Pi;
     }
 }
 
@@ -395,7 +378,7 @@ void Cyber::moveVectorSlot()
                 wheels->at(i)->setPower((round10((setAngle(-currRad) * moving * guide[i]) * qMin(speed, mx))));
             else stop(false);
         }
-        qDebug()    << (setAngle(-currRad) * moving * guide[0])\
+        qDebug()    << currRad\
                     << wheels->at(0)->getPower() << wheels->at(1)->getPower()\
                     << wheels->at(2)->getPower() << wheels->at(3)->getPower();
     }
@@ -409,7 +392,7 @@ void Cyber::calibrateSlot()
         mainTimer->stop();
         disconnect(mainTimer, SIGNAL(timeout()), this, SLOT(calibrateSlot()));
         correction = integrand / (2 * checksPerSecond);
-        qDebug() << QString::number(correction);
+        qDebug() << "Correction is:\t" + QString::number(correction);
         angles.tiltX = angles.tiltY = angles.tiltZ = \
                 absolute.tiltX = absolute.tiltY = absolute.tiltZ = \
                 currRad = integrand = count = 0;
