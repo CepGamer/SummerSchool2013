7c8a8686bfbd5ac0f5c32810e888159e5c68d81b
CepGamer <bolotov.s.s@yandex.ru>
2013-10-11 22:22:05 +0400

    fixed some things

--- TrikRider/wheel.cpp
+++ TrikRider/wheel.cpp
@@ -1,255 +1,91 @@
 /*
-Copyright (c) 2013, Bolotov Sergey
-All rights reserved.
+Copyright 2013 Bolotov Sergey
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Saint-Petersburg State University nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Bolotov Sergey BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
 #include "wheel.h"
 
-wheel::wheel(int wheelNumber, wheelType mod, QObject *parent) :
+wheel::wheel(int wheelNumber, QObject *parent) :
     QObject(parent)
 {
     wheelNum = wheelNumber;
-    cMode = mod;
-    switch (cMode) {
-    case SERV:
-        path = new QString ("/sys/class/pwm/ecap." + QString::number(wheelNum) + "/");    //  Get full path to engines
-        request = new QFile (*path + QString("request"));
-        if(!request->open(QFile::WriteOnly))
-        {
-            qDebug() << "Cannot open request #" << wheelNumber;
-        }
-        if(request->write("1") == -1 || !request->flush())
-        {
-            qDebug() << "Permission is not given to engine #" << wheelNumber;
-        }
-        period_ns = new QFile (*path + QString("period_ns"), this);
-        duty_ns = new QFile (*path + QString("duty_ns"), this);
-        run = new QFile (*path + QString("run"), this);
-        if(!(period_ns->open(QFile::WriteOnly) && duty_ns->open(QFile::WriteOnly) && run->open(QFile::WriteOnly)))
-        {
-            qDebug() << "Cannot open either run or duty or period #" << wheelNumber;
-        }
-        //  Do I need to set freq right now?
-        period_ns->write("20000000");
-        period_ns->flush();
-        //  Well...
-        break;
-    case DC:
-        message = new QString();
-        break;
-    default:
-        break;
+    QString path("/sys/class/pwm/ecap." + QString::number(wheelNum) + "/");  //  Get full path to engine
+    request = new QFile (path + QString("request"));
+
+    if(!request->open(QFile::WriteOnly))
+    {
+        qDebug() << "Cannot open request #" << wheelNumber;
     }
-    stopTimer = new QTimer (this);
-    connect(stopTimer, SIGNAL(timeout()), this, SLOT(stopSlot()));
-    stopTimer->setSingleShot(true);
+
+    if(request->write("1") == -1 || !request->flush())
+    {
+        qDebug() << "Permission is not given to engine #" << wheelNumber;
+    }
+
+    period_ns = new QFile (path + QString("period_ns"), this);
+    duty_ns = new QFile (path + QString("duty_ns"), this);
+    run = new QFile (path + QString("run"), this);
+
+    if(!(period_ns->open(QFile::WriteOnly) && duty_ns->open(QFile::WriteOnly) && run->open(QFile::WriteOnly)))
+    {
+        qDebug() << "Cannot open either run or duty or period #" << wheelNumber;
+    }
+
+    setPeriod(20000000);
 }
 
 wheel::~wheel()
 {
-    switch (cMode) {
-    case SERV:
+    if (run != NULL)
+    {
         run->write("0");
-        //  Do we need to clean other files?
         run->close();
-        period_ns->close();
-        duty_ns->close();
-        request->write("0");
-        request->close();
-        //  Delete after closing
         delete run;
-        delete duty_ns;
-        delete period_ns;
-        delete request;
-        break;
-    case DC:
-        delete message;
-        break;
     }
-    delete stopTimer;
+    if (duty_ns != NULL)
+        delete duty_ns;
+    if (period_ns != NULL)
+        delete period_ns;
+    if (request != NULL)
+    {
+        request->write("0");
+        delete request;
+    }
 }
 
 void wheel::stop()
 {
-    qDebug() << "Stop engine";
     rMode = NEUTRAL;
-    switch (cMode) {
-    case SERV:
-        run->write("0");
-        if(!run->flush())
-        {
-            qDebug() << "I can't stop! Run isn't flushing";
-        }
-        request->write("0");
-        break;
-    case DC:
-        message->clear();
-        message->append("i2cset -y 2 0x48 0x0 0x");
-        message->append(QString::number((abs((roundSpeed())) << 8) + (wheelNum << 2) + rMode, 16)); //  Number, Base
-        message->append(" w");
-        qDebug() << message;
-        system(message->toStdString().data());
-        break;
+    run->write("0");
+    if(!run->flush())
+    {
+        qDebug() << "Run isn't flushing, cannot stop.";
     }
 }
-
-/*void wheel::spinForw(qreal speed)
-{
-    if (speed > 100 || speed < -100)
-    {
-        qDebug() << "Wrong speed on wheel #" << wheelNum;
-        return ;
-    }
-    duty_ns->write(QString::number(setDutyNs (speed)).toStdString().data());
-    run->write("1");
-    run->flush();
-
-}
-
-void wheel::spinForw(qreal speed, qreal msecs)
-{
-    printf ("Spinning engine...\n");
-    if (speed > 100 || speed < 0)
-    {
-        //  We set speed in percents
-        printf("Wrong speed\n");
-    }
-    //  Debug printing
-    printf ("Writing to duty_ns %d\n", setDutyNs (speed));
-//    printf("duty_ns must be:\n%d\nBut now it:\n%d\n", (int) (setDutyNs (speed)), 1700000);
-
-    if(duty_ns->write(QString::number((setDutyNs (speed))).toStdString().data()) == -1)
-    {
-        printf ("Nothing written to duty_ns\n");
-    }
-    printf ("Writing to run...\n");
-    if(run->write("1") == -1)
-    {
-        printf("Nothing is written to run\n");
-    }
-    duty_ns->flush();
-    run->flush();
-//    printf("%s\n%s\n%s\n", period_freq->readAll().constData(), duty_ns->readAll().constData(), run->readAll().constData());
-    QTimer::singleShot((int) msecs * 1000, this, SLOT(stopSlot()));
-//    stopTimer->singleShot((int) msecs, this, SLOT(stopSlot()));
-}*/
-
-/*void wheel::spinBackw(qreal speed, qreal msecs)
-{
-
-    duty_ns->write(QString::number(setDutyNs (speed)).toStdString().data());  //  Setting speed
-    run->write("1");    //  Starting engine
-    duty_ns->flush();
-    run->flush();
-    stopTimer->singleShot((int) msecs, this, SLOT(stopSlot()));
-    //  Here'll be better func than spinForw and I'll rewrite it into upper one
-}
-
-void wheel::spinBackw(qreal speed)
-{
-    duty_ns->write(QString::number(setDutyNs (speed)).toStdString().data());
-    run->write("1");
-    duty_ns->flush();
-    run->flush();
-}*/
 
 void wheel::spin(qreal nspeed)
 {
     speed = nspeed;
-    if (speed > 100 || speed < -100)
-    {
-        qDebug() << "Wrong speed on wheel #" << wheelNum;
-        return ;
-    }
+    if (speed > 100)
+        speed = 100;
+    else if (speed < -100)
+        speed = -100;
     rMode = (speed > 0) ? FORW : BACKW;
-    switch(cMode)
-    {
-    case SERV:
-        duty_ns->write(QString::number(setDutyNs ((int) speed)).toStdString().data());
-        run->write("1");
-        duty_ns->flush();
-        run->flush();
-        break;
-    case DC:
-
-        message->clear();
-        message->append("i2cset -y 2 0x48 0x0 0x");
-        message->append(QString::number(((abs(roundSpeed())) << 8 ) + (wheelNum << 2) + rMode, 16));
-        message->append(" w");
-        //qDebug() << message->toStdString().data();
-        system(message->toStdString().data());
-
-        /*char Data[10];
-        char output[90] =  "i2cset -y 2 0x48 0x0 0x";
-        sprintf(Data,"%x", ((abs(roundSpeed())) << 8 ) + (wheelNum << 2) + rMode);
-        strcat(output,Data);
-        strcat(output," w");
-//        qDebug() << "Wheel #" << wheelNum << " speed is " << qRound(speed);
-        system(output);*/
-
-        break;
-    }
-}
-
-void wheel::spin(qreal nspeed, qreal msecs)
-{
-    speed = nspeed;
-    if (speed > 100 || speed < -100)
-    {
-        //  We set speed in percents
-        qDebug() << "Wrong speed";
-    }
-    rMode = (speed > 0) ? FORW : BACKW;
-    //  Debug printing
-    switch(cMode)
-    {
-    case SERV:
-        if(duty_ns->write(QString::number((setDutyNs ((int) speed))).toStdString().data()) == -1)
-        {
-            qDebug() << "Nothing written to duty_ns";
-        }
-        if(run->write("1") == -1)
-        {
-            qDebug () << "Nothing is written to run";
-        }
-          duty_ns->flush();
-        run->flush();
-        break;
-    case DC:
-        message->clear();
-        message->append("i2cset -y 2 0x48 0x0 0x");
-        message->append(QString::number(((abs((roundSpeed()))) << 8 ) + (wheelNum << 2) + rMode, 16));
-        message->append(" w");
-        system(message->toStdString().data());
-        break;
-    }
-    qDebug() << "Start singleshot, msecs is:\t" << msecs * 1000;
-    qDebug() << "stopTimer is singleShot: " << stopTimer->isSingleShot();
-//    QTimer::singleShot((int) msecs * 1000, this, SLOT(stopSlot()));
-//    stopTimer->singleShot((int) msecs, this, SLOT(stopSlot()));
-    stopTimer->start((int) msecs);
+    duty_ns->write(QString::number(setDutyNs ((int) speed)).toStdString().data());
+    run->write("1");
+    duty_ns->flush();
+    run->flush();
 }
 
 quint16 wheel::roundSpeed()
@@ -258,12 +94,11 @@ quint16 wheel::roundSpeed()
     speed = qRound(speed);
     speed *= 10;
     if(speed == 0)
-	rMode = NEUTRAL;
+        rMode = NEUTRAL;
     return speed;
 }
 
-void wheel::stopSlot()
+void wheel::setPeriod(quint32 per)
 {
-    qDebug() << "Entered StopSlot";
-    stop();
+    period_ns->write(QString::number(per));
 }
--- TrikRider/wheel.h
+++ TrikRider/wheel.h
@@ -1,28 +1,17 @@
 /*
-Copyright (c) 2013, Bolotov Sergey
-All rights reserved.
+Copyright 2013 Bolotov Sergey
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Saint-Petersburg State University nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Bolotov Sergey BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
 #ifndef WHEEL_H
@@ -37,7 +26,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <stdio.h>
 
-enum wheelType {SERV, DC};
 enum runMode {NEUTRAL = 0, FORW = 1, BACKW = 2, BLOCK = 3};
 
 const qreal Pi = 3.1415926535;  //  Needed for some calc's
@@ -50,11 +38,10 @@ class wheel : public QObject
 {
     Q_OBJECT
 public:
-    explicit wheel(int wheelNumber, wheelType cMode = DC, QObject *parent = 0);
+    explicit wheel(int wheelNumber, QObject *parent = 0);
     ~wheel ();                  //  Destroys the wheel
     void stop();                //  Stops
     void spin(qreal nspeed);    //  Spin continiously
-    void spin(qreal nspeed, qreal msecs);   //  Spin during some time
     inline qreal getSpeed(){return speed;}
 
 private:
@@ -63,28 +50,18 @@ private:
     QFile * period_ns;      //  Signal frequency
     QFile * duty_ns;        //  Width of 1-signal in ns
     QFile * run;            //  Run signal
-    QString * path;
-
-    //  Serie of file that controls the drives through I2C
-    QString * message;      //  System message for I2C
 
     //  Independent values
-    QTimer * stopTimer;     //  Stopping timer
     quint8 wheelNum;        //  Wheel number
     qreal speed;            //  Wheel speed (in percents)
-    wheelType cMode;   //  Connection mode
     runMode rMode;          //  Running mode
 
-//    void spinForw(float speed);     //  Precise spin
-//    void spinBackw(float speed);
-//    void spinForw(float speed, float msecs);
-//    void spinBackw(float speed, float msecs);
     inline int setDutyNs(int nspeed){return zeroPoint + (nspeed * amplitude) / 100;} //  Convert percents into nsecs
     quint16 roundSpeed();   //  Round speed to degree of 10
+    void setPeriod (quint32 per);
 signals:
 
 public slots:
-    void stopSlot();
 };
 
 #endif // WHEEL_H
